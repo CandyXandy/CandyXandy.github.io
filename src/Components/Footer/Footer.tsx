bd778fd812907f477435abebdd78fb3032600451
CandyXandy <gifmesalad@gmail.com>
2025-04-12 21:12:25 +0930

    [FEAT] removed unnecessary elements from footer.

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,35 +1,19 @@
 import './Footer.css'
 import footer_logo from '../../assets/logo.svg'
-import user_icon from '../../assets/user_icon.svg'
+
+
 
 
 const Footer = () => {
     return (
         <div className='footer'>
+            <hr />
             <div className="footer-top">
-                <div className="footer-top-left">
+                <div className="footer-left">
                     <img id='logoPic' src={footer_logo} alt="" />
                     <p>Alexander Robertson. Software Engineer. Adelaide, South Australia.</p>
                 </div>
-                <div className="footer-top-right">
-                    <div className="footer-email-input">
-                        <img src={user_icon} alt="" />
-                        <input type="email" placeholder='Enter your email' />
-                    </div>
-                    <div className="footer-subscribe">Subscribe</div>
-                </div>
-            </div>
-
-
-            <hr />
-
-            <div className="footer-bot">
-                <p className="footer-bottom-left"> @ 2025 Alexander Robertson. All Rights Reserved.</p>
-                <div className="footer-bottom-right">
-                    <p>Terms of Service</p>
-                    <p>Privacy Policy</p>
-                    <p>Connect with me</p>
-                </div>
+                <p className="footer-right"> @ 2025 Alexander Robertson. All Rights Reserved.</p>
             </div>
         </div>
     )
