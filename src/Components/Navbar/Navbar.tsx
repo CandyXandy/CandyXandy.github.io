336354c37994f33b88ebea277a8fe824707d1181
CandyXandy <gifmesalad@gmail.com>
2025-04-12 21:16:45 +0930

    [FIX] fixed offsets for smooth scroll

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -26,12 +26,12 @@ const Navbar = () => {
             <ul ref={menuRef} className="nav-menu">
                 <img src={menuClose} onClick={closeMenu} alt="" className="nav-mob-close" />
                 <li><AnchorLink className='anchor-link' href='#home'><p onClick={() => setMenu("home")}>Home</p></AnchorLink>{menu === "home" ? <img src={underline} alt='' /> : <></>}</li>
-                <li><AnchorLink className='anchor-link' offset={50} href='#about'><p onClick={() => setMenu("about")}>About Me</p></AnchorLink>{menu === "about" ? <img src={underline} alt='' /> : <></>}</li>
-                <li><AnchorLink className='anchor-link' offset={50} href='#resume'><p onClick={() => setMenu("resume")}>Resume</p></AnchorLink>{menu === "resume" ? <img src={underline} alt='' /> : <></>}</li>
-                <li><AnchorLink className='anchor-link' offset={50} href='#portfolio'><p onClick={() => setMenu("portfolio")}>Portfolio</p></AnchorLink>{menu === "portfolio" ? <img src={underline} alt='' /> : <></>}</li>
-                <li><AnchorLink className='anchor-link' offset={50} href='#contact'><p onClick={() => setMenu("contact")}>Contact</p></AnchorLink>{menu === "contact" ? <img src={underline} alt='' /> : <></>}</li>
+                <li><AnchorLink className='anchor-link' offset={80} href='#about'><p onClick={() => setMenu("about")}>About Me</p></AnchorLink>{menu === "about" ? <img src={underline} alt='' /> : <></>}</li>
+                <li><AnchorLink className='anchor-link' offset={80} href='#resume'><p onClick={() => setMenu("resume")}>Resume</p></AnchorLink>{menu === "resume" ? <img src={underline} alt='' /> : <></>}</li>
+                <li><AnchorLink className='anchor-link' offset={80} href='#portfolio'><p onClick={() => setMenu("portfolio")}>Portfolio</p></AnchorLink>{menu === "portfolio" ? <img src={underline} alt='' /> : <></>}</li>
+                <li><AnchorLink className='anchor-link' offset={80} href='#contact'><p onClick={() => setMenu("contact")}>Contact</p></AnchorLink>{menu === "contact" ? <img src={underline} alt='' /> : <></>}</li>
             </ul>
-            <div className="nav-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect With Me</AnchorLink></div>
+            <div className="nav-connect"><AnchorLink className='anchor-link' offset={80} href='#contact'>Connect With Me</AnchorLink></div>
         </div>
     )
 }
